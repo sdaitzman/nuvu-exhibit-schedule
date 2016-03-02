3626e0b22aac7b44d92552fb3696385be975e6a8
Louie Adamian <louieadamian@gmail.com>
2016-03-02 09:34:56 -0500

    fixes app.js

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
+@@ -1,4 +1,6 @@
 #!/usr/bin/env node
 var express = require('express')
 var app = express()
 var exhibits = require('./data/exhibits.json')
 app.set('view engine', 'jade')
 app.use(express.static('www'))
-
-app.get('/', (req, res) => {
-  res.render('index', {
-    "title": 'NuVu Exhibit Schedule',
-    "exhibits": exhibits
-  })
-})
-
-app.listen(3000, () => {
-  console.log('NuVu Exhibit Schedule listening')
-})
