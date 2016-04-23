7b533ff1909548cd8fc929a12d2f558ff6ac1476
Louie Adamian <louieadamian@gmail.com>
2016-04-22 22:07:36 -0400

    trying to fade text
    not functional

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var exhibits = require('./data/exhibits.json')
 _.forEach(exhibits, (e) => {
   e.students = e.students.toUpperCase()
   e.title = e.title.toUpperCase()
+  $(document).ready(function(){
+      $("div").click(function(){
 })
 
 app.set('view engine', 'jade')
@@ -24,5 +26,7 @@ if (app.get('env') === 'production') appPort = 80
 
 console.log(appPort)
 app.listen(appPort, () => {
-  console.log('NuVu Exhibit Schedule listening')
+  console.log('NuVu Exhibit Schedule is listening')
+  console.log('NuVu Exhibit Schedule is watching')
+
 })
